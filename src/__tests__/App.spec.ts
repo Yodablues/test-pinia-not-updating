70df4007024fd4161f340e6d45af3ebb3909d40d
Tom Colarusso <tom@verivend.com>
2025-04-14 13:19:39 -0400

    clean up tests

--- src/__tests__/App.spec.ts
+++ src/__tests__/App.spec.ts
@@ -3,28 +3,9 @@ import App from '../App.vue'
 import { flushPromises, mount } from '@vue/test-utils'
 import { RouterLinkStub } from '@vue/test-utils'
 import { createTestingPinia } from '@pinia/testing'
-import { http, HttpResponse } from 'msw'
-import { setupServer } from 'msw/node'
 import { useUserStore } from '@/stores/userStore'
 
 describe('App', () => {
-  const restHandlers = [
-    http.get('https://localhost:5001/api/user/simple/', () => {
-      return HttpResponse.json({ user: { id: 1, name: 'John Doe' } })
-    }),
-  ]
-
-  const server = setupServer(...restHandlers)
-
-  // Start server before all tests
-  beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
-
-  // Close server after all tests
-  afterAll(() => server.close())
-
-  // Reset handlers after each test for test isolation
-  afterEach(() => server.resetHandlers())
-
   beforeEach(() => {
     window.localStorage.setItem('app_accessToken', 'asd')
     window.localStorage.setItem('app_refreshToken', 'asd')
